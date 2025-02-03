ef350836f957f7e59e28d53f988c66b72c3bac5d
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2025-02-03 12:22:24 +0100

    chapter 5

---
+++ app/ui/font.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import { Lexend_Giga } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lexendGiga = Lexend_Giga({ subsets: ['latin'] });
